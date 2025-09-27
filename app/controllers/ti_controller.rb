dd439a2ae7c728fd7f8a6d416295a599f2c87495
黒田 <ddki313kr2@gmail.com>
2025-09-27 19:01:53 +0900

    Revert "feat: add question3 page"
    This reverts commit 4b83178f1af8d8234370e08a4bffbd1e2c034175.

--- app/controllers/ti_controller.rb
+++ app/controllers/ti_controller.rb
@@ -7,13 +7,7 @@ class TiController < ApplicationController
 
   def question2
     # クエリパラメーターからq1を読み込む
-    @params1 = params[:q1]
-  end
-
-  def question3
-    @params1 = params[:q1]
-    puts @params1
-    @params2 = params[:q2]
+    @params = params[:q1]
   end
 
 
