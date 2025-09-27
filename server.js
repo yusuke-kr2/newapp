2dd7334da7be6551789e546aa0b957613fe353a4
黒田 <ddki313kr2@gmail.com>
2025-09-27 13:13:45 +0900

    kuroda is kuzeu

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.get("/", (req, res) => {
   res.send("Hello World from Render + Docker!");
 });
 
+app.get("/kuroda", (req, res) => {
+  res.send("私はクズです。");
+});
+
 app.listen(3000, () => console.log("Server running on port 3000"));
